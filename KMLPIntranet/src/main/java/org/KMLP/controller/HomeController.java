5895150ea4db5cfe089fbfa4bed10605eeed143c
user <user@175.205.193.249>
2017-07-20 16:40:06 +0900

    설정 파일 수정

--- KMLPIntranet/src/main/java/org/KMLP/controller/HomeController.java
+++ KMLPIntranet/src/main/java/org/KMLP/controller/HomeController.java
@@ -3,13 +3,13 @@ package org.KMLP.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Handles requests for the application home page.
@@ -22,9 +22,9 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("home PAGE...............");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -33,7 +33,39 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginGET(Model model) {
+
+		logger.info("loginGET PAGE...............");
+
+		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginPOST(Model model, RedirectAttributes rttr) throws Exception {
+
+		logger.info("loginPOST post ...........");
+
+		return "redirect:/notice/notice_list";
+	}
+	
+	@RequestMapping(value = "/mypage_modify", method = RequestMethod.GET)
+	public String mypageGET(Model model) {
+
+		logger.info("mypageGET PAGE...............");
+
+		return "mypage_modify";
+	}
+
+	@RequestMapping(value = "/mypage_modify", method = RequestMethod.POST)
+	public String mypagePOST(Model model) throws Exception {
+
+		logger.info("mypagePOST post ...........");
+
+		return "redirect:/notice/notice_list";
 	}
 	
 }
--- KMLPIntranet/src/main/java/org/KMLP/controller/NoticeController.java
+++ KMLPIntranet/src/main/java/org/KMLP/controller/NoticeController.java
@@ -1,5 +1,79 @@
 package org.KMLP.controller;
 
-public class NoticeController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/notice")
+public class NoticeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NoticeController.class);
+	
+	@RequestMapping(value = "/notice_list", method = RequestMethod.GET)
+	public String notice_ListGET(Model model) {
+
+		logger.info("notice_ListGET PAGE...............");
+
+		return "notice_list";
+	}
+
+	@RequestMapping(value = "/notice_list", method = RequestMethod.POST)
+	public String notice_ListPOST(Model model) throws Exception {
+
+		logger.info("notice_ListPOST post ...........");
+
+		return "notice_list";
+	}
+	
+	@RequestMapping(value = "/notice_content", method = RequestMethod.GET)
+	public String notice_ContentGET(Model model) {
+
+		logger.info("notice_ContentGET PAGE...............");
+
+		return "notice_content";
+	}
+
+	@RequestMapping(value = "/notice_content", method = RequestMethod.POST)
+	public String notice_ContentPOST(Model model) throws Exception {
+
+		logger.info("notice_ContentPOST post ...........");
+
+		return "notice_content";
+	}
+	
+	@RequestMapping(value = "/notice_modify", method = RequestMethod.GET)
+	public String notice_ModifyGET(Model model) {
+
+		logger.info("notice_ModifyGET PAGE...............");
+
+		return "notice_modify";
+	}
+
+	@RequestMapping(value = "/notice_modify", method = RequestMethod.POST)
+	public String notice_ModifyPOST(Model model) throws Exception {
+
+		logger.info("notice_ModifyPOST post ...........");
+
+		return "notice_modify";
+	}
+	
+	@RequestMapping(value = "/notice_regist", method = RequestMethod.GET)
+	public String notice_RegistGET(Model model) {
+
+		logger.info("notice_RegistGET PAGE...............");
+
+		return "notice_regist";
+	}
+
+	@RequestMapping(value = "/notice_regist", method = RequestMethod.POST)
+	public String notice_RegistPOST(Model model) throws Exception {
+
+		logger.info("notice_RegistPOST post ...........");
+
+		return "notice_regist";
+	}
 }
