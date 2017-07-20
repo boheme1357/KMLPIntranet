6ac997aa78b23a0a2bc87f15abf0f341c384c757
user <user@175.205.193.249>
2017-07-20 17:47:44 +0900

    ddd

--- KMLPIntranet/src/main/java/org/KMLP/controller/HomeController.java
+++ KMLPIntranet/src/main/java/org/KMLP/controller/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * 기본 컨트롤러
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
