7f71ec5f44dedf422b0dd5ff012358eebbf812e6
user <user@175.205.193.249>
2017-07-31 17:46:31 +0900

    회원가입한 아이디로 로그인 구현

--- KMLPIntranet/src/main/java/org/KMLP/persistence/MemberDAOImpl.java
+++ KMLPIntranet/src/main/java/org/KMLP/persistence/MemberDAOImpl.java
@@ -13,68 +13,68 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MemberDAOImpl implements MemberDAO {
 
-	private static final String namespace="memberMapper";
-	
+	private static final String namespace = "memberMapper";
+
 	@Inject
 	private SqlSession sqlSession;
-	
-	
-	//01. 사원전체목록
+
+	// 01. 사원전체목록
 	@Override
 	public List<MemberVO> selectAll() {
-		return sqlSession.selectList(namespace+".selectAll");
+		return sqlSession.selectList(namespace + ".selectAll");
 	}
 
-	//02_02. 사원데이터삽입
+	// 02_02. 사원데이터삽입
 	@Override
 	public void insert(MemberVO vo) {
-		sqlSession.insert(namespace+".insert", vo);
-	}
+		sqlSession.insert(namespace + ".insert", vo);
+		sqlSession.insert(namespace + ".insertAuthority", vo);
 
-	
-	//03. 사원개인정보조회
-	@Override
-	public MemberVO selectContent(String m_id) {
-		return sqlSession.selectOne(namespace+".selectContent", m_id);
-	}
+		try {
+			System.out.println("-----insertSignImg------------------");
+			sqlSession.insert(namespace + ".insertSignImg", vo);
 
-	//04. 사원정보수정
-	@Override
-	public void update(MemberVO vo) {
-		sqlSession.update(namespace+".update", vo);
-	}
-	
-	
-	//05. 사원정보삭제
-		@Override
-		public void delete(String m_id) {
-			sqlSession.delete(namespace+".delete", m_id);
+		} catch (NullPointerException e) {
+			System.out.println("도장 이미지 미등록 ...........");
+			return;
 		}
 
-		
-	
-	//06. 비밀번호체크
+	}
+
+	// 03. 사원개인정보조회
+	@Override
+	public MemberVO selectContent(String m_id) {
+		return sqlSession.selectOne(namespace + ".selectContent", m_id);
+	}
+
+	// 04. 사원정보수정
+	@Override
+	public void update(MemberVO vo) {
+		sqlSession.update(namespace + ".update", vo);
+	}
+
+	// 05. 사원정보삭제
+	@Override
+	public void delete(String m_id) {
+		sqlSession.delete(namespace + ".delete", m_id);
+	}
+
+	// 06. 비밀번호체크
 	@Override
 	public boolean checkPw(String m_id, String m_pwd) {
-		
-		boolean result=false;
-		
-		Map<String, String> map=new HashMap<String, String>();
+
+		boolean result = false;
+
+		Map<String, String> map = new HashMap<String, String>();
 		map.put("m_id", m_id);
 		map.put("m_pwd", m_pwd);
-		
-		int count = sqlSession.selectOne(namespace+".checkPw", map);
-		
-		if(count==1) result=true;
-		
+
+		int count = sqlSession.selectOne(namespace + ".checkPw", map);
+
+		if (count == 1)
+			result = true;
+
 		return result;
 	}
-	
-
-												
-	
-	
-	
-	
 
 }
