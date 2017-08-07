aa1e30e72ef287b9b10d7587cab1ef1ad136da36
user <user@175.205.193.249>
2017-08-07 19:58:02 +0900

    설정파일 정리 및 버전업

--- KMLPIntranet/src/main/java/org/KMLP/util/ChatWebSocketHandler.java
+++ KMLPIntranet/src/main/java/org/KMLP/util/ChatWebSocketHandler.java
@@ -16,31 +16,31 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionEstablished(
 			WebSocketSession session) throws Exception {
-		log(session.getId() + " 연결 됨");
+		log(session.getId() + " afterConnectionEstablished 연결 됨");
 		users.put(session.getId(), session);
 	}
 
 	@Override
 	public void afterConnectionClosed(
 			WebSocketSession session, CloseStatus status) throws Exception {
-		log(session.getId() + " 연결 종료됨");
+		log(session.getId() + " afterConnectionClosed 연결 종료됨");
 		users.remove(session.getId());
 	}
 
 	@Override
 	protected void handleTextMessage(
 			WebSocketSession session, TextMessage message) throws Exception {
-		log(session.getId() + "로부터 메시지 수신: " + message.getPayload());
+		log(session.getId() + "로부터 메시지 수신 handleTextMessage: " + message.getPayload());
 		for (WebSocketSession s : users.values()) {
 			s.sendMessage(message);
-			log(s.getId() + "에 메시지 발송: " + message.getPayload());
+			log(s.getId() + "에 메시지 발송 handleTextMessage: " + message.getPayload());
 		}
 	}
 
 	@Override
 	public void handleTransportError(
 			WebSocketSession session, Throwable exception) throws Exception {
-		log(session.getId() + " 익셉션 발생: " + exception.getMessage());
+		log(session.getId() + " 익셉션 발생 handleTransportError: " + exception.getMessage());
 	}
 
 	private void log(String logmsg) {
